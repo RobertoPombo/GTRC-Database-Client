9656e5460aef93c18d703b1feaace8c8f040ae49
RobertoPombo <captn.sponge2@web.de>
2024-01-21 02:23:45 +0100

    Added DbApiConnectionConfig.LoadJson(), .SaveJson(), .Connectivity().

--- DbApiConnectionConfig.cs
+++ DbApiConnectionConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Net.Http.Json;
+using System.Text;
 
 using GTRC_Basics;
 using GTRC_Basics.Models;
@@ -9,8 +10,14 @@ namespace GTRC_Database_Client
     public class DbApiConnectionConfig : ConnectionConfig
     {
         public static readonly List<DbApiConnectionConfig> List = [];
+        private static readonly string path = GlobalValues.DataDirectory + "config dbapi.json";
 
-        public DbApiConnectionConfig() { List.Add(this); Name = name; UpdateDbApiRequests(); }
+        public DbApiConnectionConfig()
+        {
+            List.Add(this);
+            Name = name;
+            UpdateDbApiRequests();
+        }
 
         private string name = "Preset #1";
         private bool isActive = false;
@@ -40,28 +47,6 @@ namespace GTRC_Database_Client
             set { if (value != isActive) { if (value) { DeactivateAllConnections(); } isActive = value; } }
         }
 
-        public bool IsUniqueName()
-        {
-            int listIndexThis = List.IndexOf(this);
-            for (int conNr = 0; conNr < List.Count; conNr++)
-            {
-                if (List[conNr].Name == name && conNr != listIndexThis) { return false; }
-            }
-            return true;
-        }
-
-        public static DbApiConnectionConfig? GetActiveConnection()
-        {
-            foreach (DbApiConnectionConfig con in List) { if (con.IsActive) { con.UpdateDbApiRequests(); return con; } }
-            return null;
-        }
-
-        public static void DeactivateAllConnections()
-        {
-            DbApiConnectionConfig? con = GetActiveConnection();
-            if (con is not null) { con.IsActive = false; }
-        }
-
         [JsonIgnore] public string BaseUrl
         {
             get
@@ -77,7 +62,6 @@ namespace GTRC_Database_Client
 
         public async Task<HttpResponseMessage?> SendRequest(string modelTypename, HttpRequestType requestType, string? path = null, dynamic? objDto = null)
         {
-            HttpResponseMessage? response = null;
             path ??= string.Empty;
             string url = string.Join("/", [BaseUrl, modelTypename, requestType.ToString()]);
             using HttpClient httpClient = new();
@@ -87,22 +71,95 @@ namespace GTRC_Database_Client
                     switch (requestType)
                     {
                         case HttpRequestType.Get:
-                            if (objDto is null) { response = await httpClient.GetAsync(url + path); }
-                            else { response = await httpClient.PutAsync(url + path, JsonContent.Create(objDto)); }
-                            break;
+                            if (objDto is null) { return await httpClient.GetAsync(url + path); }
+                            else { return await httpClient.PutAsync(url + path, JsonContent.Create(objDto)); }
                         case HttpRequestType.Delete:
-                            if (objDto is null) { response = await httpClient.DeleteAsync(url + path); }
-                            else { response = await httpClient.DeleteAsync(url + path, JsonContent.Create(objDto)); }
-                            break;
-                        case HttpRequestType.Add: if (objDto is not null) { response = await httpClient.PostAsync(url + path, JsonContent.Create(objDto)); } break;
-                        case HttpRequestType.Update: if (objDto is not null) { response = await httpClient.PutAsync(url + path, JsonContent.Create(objDto)); } break;
-                        default: response = null; break;
+                            if (objDto is null) { return await httpClient.DeleteAsync(url + path); }
+                            else { return await httpClient.DeleteAsync(url + path, JsonContent.Create(objDto)); }
+                        case HttpRequestType.Add: if (objDto is not null) { return await httpClient.PostAsync(url + path, JsonContent.Create(objDto)); } break;
+                        case HttpRequestType.Update: if (objDto is not null) { return await httpClient.PutAsync(url + path, JsonContent.Create(objDto)); } break;
+                        default: return null;
                     }
                 }
-                catch (HttpRequestException) { GlobalValues.CurrentLogText = "Connection to GTRC-Database-API failed!"; }
+                catch { }
+                GlobalValues.CurrentLogText = "Connection to GTRC-Database-API failed!";
+                return null;
             }
-            return response;
         }
+
+        public bool IsUniqueName()
+        {
+            int listIndexThis = List.IndexOf(this);
+            for (int conNr = 0; conNr < List.Count; conNr++)
+            {
+                if (List[conNr].Name == name && conNr != listIndexThis) { return false; }
+            }
+            return true;
+        }
+
+        public async Task<bool> Connectivity()
+        {
+            foreach (Type modelType in GlobalValues.ModelTypeList)
+            {
+                HttpResponseMessage? Response = await SendRequest(modelType.Name, HttpRequestType.Get);
+                if (Response is null) { return false; }
+            }
+            return true;
+        }
+
+        public static void LoadJson()
+        {
+            if (!Directory.Exists(GlobalValues.DataDirectory)) { Directory.CreateDirectory(GlobalValues.DataDirectory); }
+            if (!File.Exists(path)) { File.WriteAllText(path, JsonConvert.SerializeObject(List, Formatting.Indented), Encoding.Unicode); }
+            try
+            {
+                List.Clear();
+                _ = JsonConvert.DeserializeObject<List<DbApiConnectionConfig>>(File.ReadAllText(path, Encoding.Unicode)) ?? [];
+                GlobalValues.CurrentLogText = "GTRC-Database-API connection settings restored.";
+            }
+            catch { GlobalValues.CurrentLogText = "Restore GTRC-Database-API connection settings failed!"; }
+            if (List.Count == 0) { _ = new DbApiConnectionConfig(); }
+        }
+
+        public static void SaveJson()
+        {
+            string text = JsonConvert.SerializeObject(List, Formatting.Indented);
+            File.WriteAllText(path, text, Encoding.Unicode);
+            GlobalValues.CurrentLogText = "GTRC-Database-API connection settings saved.";
+        }
+
+        public static DbApiConnectionConfig? GetActiveConnection()
+        {
+            foreach (DbApiConnectionConfig con in List) { if (con.IsActive) { con.UpdateDbApiRequests(); return con; } }
+            return null;
+        }
+
+        public static void DeactivateAllConnections()
+        {
+            DbApiConnectionConfig? con = GetActiveConnection();
+            if (con is not null) { con.IsActive = false; }
+        }
+
+        [JsonIgnore] public DbApiRequest<Color> Color { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Sim> Sim { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<User> User { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Track> Track { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Carclass> Carclass { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Manufacturer> Manufacturer { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Car> Car { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Role> Role { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<UserRole> UserRole { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Bop> Bop { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<BopTrackCar> BopTrackCar { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Series> Series { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Season> Season { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<SeasonCarclass> SeasonCarclass { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Organization> Organization { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<OrganizationUser> OrganizationUser { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Team> Team { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<Event> Event { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<EventCarclass> EventCarclass { get; set; } = new();
+        [JsonIgnore] public DbApiRequest<EventCar> EventCar { get; set; } = new();
 
         public void UpdateDbApiRequests()
         {
@@ -127,26 +184,5 @@ namespace GTRC_Database_Client
             EventCarclass = new(this);
             EventCar = new(this);
         }
-
-        [JsonIgnore] public DbApiRequest<Color> Color { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Sim> Sim { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<User> User { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Track> Track { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Carclass> Carclass { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Manufacturer> Manufacturer { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Car> Car { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Role> Role { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<UserRole> UserRole { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Bop> Bop { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<BopTrackCar> BopTrackCar { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Series> Series { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Season> Season { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<SeasonCarclass> SeasonCarclass { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Organization> Organization { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<OrganizationUser> OrganizationUser { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Team> Team { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<Event> Event { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<EventCarclass> EventCarclass { get; set; } = new();
-        [JsonIgnore] public DbApiRequest<EventCar> EventCar { get; set; } = new();
     }
 }
