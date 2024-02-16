05bd1f7edd9311b9c1620009406fd2b35e347f15
RobertoPombo <captn.sponge2@web.de>
2024-02-16 18:01:32 +0100

    Moved DbApi from GTRC-Web-Backend to GTRC_Database_Client.

---
+++ DbApi.cs
@@ -0,0 +1,15 @@
+﻿using GTRC_Database_Client;
+
+namespace GTRC_Web_Backend
+{
+    public class DbApi
+    {
+        public static readonly DbApiConnectionConfig Connection;
+
+        static DbApi()
+        {
+            DbApiConnectionConfig.LoadJson();
+            Connection = DbApiConnectionConfig.GetActiveConnection() ?? DbApiConnectionConfig.List[0];
+        }
+    }
+}
