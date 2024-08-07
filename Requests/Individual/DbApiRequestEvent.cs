a209682d90e1abfc40ac3f5bbdb0750539f2f98a
RobertoPombo <captn.sponge2@web.de>
2024-08-08 00:26:27 +0200

    Bugfix DateTime.Now.

--- Requests/Individual/DbApiRequestEvent.cs
+++ Requests/Individual/DbApiRequestEvent.cs
@@ -14,7 +14,7 @@ namespace GTRC_Database_Client.Requests
 
         public async Task<DbApiObjectResponse<Event>> GetNext(int seasonId, DateTime? date = null)
         {
-            string _date = (date ?? DateTime.UtcNow).ToString("MM/dd/yyyy HH:mm:ss");
+            string _date = (date ?? DateTime.Now).ToString("MM/dd/yyyy HH:mm:ss");
             if (connection is not null) { Response = await connection.SendRequest(Model, HttpRequestType.Get, "/Next/" + seasonId.ToString(), _date, nameof(date)); }
             return await ReturnAsObject(Response);
         }
